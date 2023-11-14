4c2650ed427c14b29e8a457611e8185b428effa6
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-14 21:32:28 +0000

    created PriceScraper class

---
+++ app/services/price_scraper.rb
@@ -0,0 +1,12 @@
+
+class PriceScraper
+  # attr_accessor :product_id
+
+  # def initialize(product_id)
+  #   @product_id = product_id
+  # end
+
+  def scrape
+    # TODO: implement price scraping
+  end
+end
