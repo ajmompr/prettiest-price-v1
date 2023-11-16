4bcbe9ebf3fc4494dc036e9370d8573c2a71705b
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-16 20:55:58 +0000

    added #TODO comments to my listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -13,4 +13,20 @@ class Listing < ApplicationRecord
   belongs_to :product, required: true, class_name: "Product", foreign_key: "product_id"
   belongs_to :retailer, required: true, class_name: "Retailer", foreign_key: "retailer_id"
   has_many  :snapshots, class_name: "Snapshot", foreign_key: "listing_id", dependent: :destroy
+
+  # def fetch_snapshot
+    # TODO: add listing_id
+    # something like this 
+    # if retailer.name == "amazon"
+      # price = AmazonPriceScraper.new(retailer_id).scrape
+    # elsif retailer.name = "walmart"
+      # TODO: walmart, etc....
+    # end
+  # end
+
+  # def self.fetch_snapshots
+  #   Listing.all.each do |listing|
+  #     listing.fetch_snapshot
+  #   end
+  # end
 end
