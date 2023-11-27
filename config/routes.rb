de02dd9cf14b2166926d0829b254e8db853e7577
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-27 16:34:11 +0000

    uncommented out/re-added listings, retailers, snapshots, and products resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get '/404', to: 'errors#not_found'
   get '/500', to: 'errors#internal_server_error'
-  # resources :listings
-  # resources :retailers
-  # resources :snapshots
-  # resources :products
+  resources :listings
+  resources :retailers
+  resources :snapshots
+  resources :products
   
   resources :waitlist_entries, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
