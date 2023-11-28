2dbddcd68476cae74bc847dc8585f611e635f9fe
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-28 16:45:19 +0000

    edited and migrated devise users

--- db/migrate/20231128162359_devise_create_users.rb
+++ db/migrate/20231128162359_devise_create_users.rb
@@ -33,16 +33,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
       t.string :username
-      t.string :email
-      t.string :password
       t.string :avatar
-      t.integer :fave_count
+      t.integer :fave_count, default: 0
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :username,             unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
