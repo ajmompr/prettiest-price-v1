5afb9fe5fe545e9d0ce8f37aa373df5da269e31b
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-28 21:07:21 +0000

    added admin column to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
+#  admin                  :boolean          default(FALSE), not null
 #  avatar                 :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
