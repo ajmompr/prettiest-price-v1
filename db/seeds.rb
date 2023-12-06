3551eefdd906f0f9e6d5114a94665c27bf9c3ae4
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-12-06 20:25:36 +0000

    created new MVP product record to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,16 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # TODO: create admin user(s) here
-require 'securerandom'
+require "securerandom"
 
 random_string = SecureRandom.hex(8)
 
 robohash_url = 'https://robohash.org/#{random_string}.png?set=set4'
 
-admin_password = ENV['TPP_ADMIN']
-User.create(admin: true, avatar: robohash_url, email: 'amomprem@uillinois.edu', password: admin_password, username: 'amomprem')
+admin_password = ENV["TPP_ADMIN"]
+User.create(admin: true, avatar: robohash_url, email: "amomprem@uillinois.edu", password: admin_password, username: "amomprem")
+
+# Create MVP Product Here
+Product.create(description: "CeraVe Moisturizing Cream is a rich, non-greasy, fast-absorbing moisturizer for normal to dry skin on the face and body.", image: "app/assets/images/cerave_bodycream_16_oz.png", name: "CeraVe Moisturizing Cream", product_volume: 16)
 
 # TODO: create retailer(s) here
