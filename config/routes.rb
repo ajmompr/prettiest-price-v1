653f2faed56716c8608f241c8c18716727348083
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-12-11 08:30:04 +0000

    commented out root to    "devise/sessions#new"

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
   devise_for :users
 
     # root to: "devise/sessions#new"
+    # devise_scope :user do
+    #   root to: "devise/sessions#new"
+    # end
+
  root to: "static#index"
- 
+
   get "/404", to: "errors#not_found"
   get "/500", to: "errors#internal_server_error"
 
