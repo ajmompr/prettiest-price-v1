caccded9f6fb203a5bde9e75a64cd31aa21d2dfe
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-12-13 06:12:07 +0000

    updated routes & removed unncessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,28 @@
 Rails.application.routes.draw do
-  # TODO: restrict to only admins
   authenticate :user, ->(user) { user.admin? } do
     mount RailsAdmin::Engine, at: "admin", as: "rails_admin"
   end
-  
+
   devise_for :users
 
-    # root to: "devise/sessions#new"
-    # devise_scope :user do
-    #   root to: "devise/sessions#new"
-    # end
-
- root to: "products#index"
+  root to: "products#index"
 
   get "/404", to: "errors#not_found"
   get "/500", to: "errors#internal_server_error"
 
-  resources :listings
-  resources :retailers
-  resources :snapshots do
+  resources :listings, only: :index
+  resources :retailers, only: :index
+  resources :snapshots, only: :index do
     collection do
       get "snaps_by_day"
     end
   end
-  resources :products
-  resources :waitlist_entries, only: [:create]
-
+  resources :products, only: [:index, :show]
+  resources :waitlist_entries, only: :create
 
   if Rails.env.development?
     mount RailsDb::Engine => "/rails/db", :as => "rails_db_admin"
   end
-
-  # Defines the root path route ("/") 
-
 
   match "*umatched_route", to: "errors#not_found", via: :all
 end
