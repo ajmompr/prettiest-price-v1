a88602cdada5083aacf712e751ae07eafc9ecd14
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-12-13 07:01:16 +0000

    removed snaps#index action

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,10 +1,5 @@
 class SnapshotsController < ApplicationController
-  # GET /snapshots or /snapshots.json
-  def index
-    @snapshots = Snapshot.all
-  end
-
- 
+#  Render json for snaps chart
   def snaps_by_day
     render json: Snapshot.where(listing_id: params[:listing_id]).group_by_day(:snapshot_date).sum(:price)
   end
