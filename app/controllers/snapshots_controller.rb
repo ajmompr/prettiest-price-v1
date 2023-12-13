3834da2f3a70c327923395050a117922aa410d82
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-12-13 06:59:36 +0000

    removed unncessary actions from snaps controller

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,62 +1,10 @@
 class SnapshotsController < ApplicationController
-  before_action :set_snapshot, only: %i[ show edit update destroy ]
-
   # GET /snapshots or /snapshots.json
   def index
     @snapshots = Snapshot.all
   end
 
-  # GET /snapshots/1 or /snapshots/1.json
-  def show
-  end
-
-  # GET /snapshots/new
-  def new
-    @snapshot = Snapshot.new
-  end
-
-  # GET /snapshots/1/edit
-  def edit
-  end
-
-  # POST /snapshots or /snapshots.json
-  def create
-    @snapshot = Snapshot.new(snapshot_params)
-
-    respond_to do |format|
-      if @snapshot.save
-        format.html { redirect_to snapshot_url(@snapshot), notice: "Snapshot was successfully created." }
-        format.json { render :show, status: :created, location: @snapshot }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @snapshot.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /snapshots/1 or /snapshots/1.json
-  def update
-    respond_to do |format|
-      if @snapshot.update(snapshot_params)
-        format.html { redirect_to snapshot_url(@snapshot), notice: "Snapshot was successfully updated." }
-        format.json { render :show, status: :ok, location: @snapshot }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @snapshot.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /snapshots/1 or /snapshots/1.json
-  def destroy
-    @snapshot.destroy
-
-    respond_to do |format|
-      format.html { redirect_to snapshots_url, notice: "Snapshot was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
+ 
   def snaps_by_day
     render json: Snapshot.where(listing_id: params[:listing_id]).group_by_day(:snapshot_date).sum(:price)
   end
