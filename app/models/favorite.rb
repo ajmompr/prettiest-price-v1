99738822026c3c73dede25acd698fc44586d6797
ajmompr <amompremier@gmail.com>
2024-02-15 15:58:24 -0600

    added direct associations (product & user) to favorites model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -9,4 +9,6 @@
 #  user_id    :integer
 #
 class Favorite < ApplicationRecord
+  belongs_to :product, class_name: "Product", foreign_key: "product_id"
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
 end
