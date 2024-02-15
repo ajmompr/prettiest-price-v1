88964a208f269833fdd4631ed7435100fe64a080
ajmompr <amompremier@gmail.com>
2024-02-15 15:55:01 -0600

    re-ordered routes/moved favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-  resources :favorites
   authenticate :user, ->(user) { user.admin? } do
     mount Blazer::Engine, at: "blazer"
     mount RailsAdmin::Engine, at: "admin", as: "rails_admin"
   end
-
+  
   devise_for :users
-
+  
   root to: "products#index"
-
+  
   get "/404", to: "errors#not_found"
   get "/500", to: "errors#internal_server_error"
-
-
+  
+  
   resources :retailers, only: :index
   resources :products, only: [:index, :show]
+  resources :favorites
   resources :waitlist_entries, only: :create
-  
+
   resources :snapshots, only: [] do
     collection do
       get "snaps_by_day"
